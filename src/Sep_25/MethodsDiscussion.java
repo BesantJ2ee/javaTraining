8a4a5a937dcd973ebb9450db7c9c87c4b36651f1
BesantJ2ee <simonjustin321@gmail.com>
2022-09-25 21:08:35 +0530

    first code for method discussion

--- src/Sep_25/MethodsDiscussion.java
+++ src/Sep_25/MethodsDiscussion.java
@@ -1,10 +1,38 @@
 package Sep_25;
 
 public class MethodsDiscussion {
-
+	//Instance variable
+	int a =10;
+	int b=20;
+    //Method name
+	void nellore()
+	{
+		//Variable used inside the method, and the scope is inside only
+		//its called local variable
+		int c=a+b;
+		System.out.println(c);
+	}
+	//method name
+	//Void means which will not return anything
+	void andhrapradesh()
+	{
+		int c=b-a;
+		System.out.println(c);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		//Class Name      //Ref Variable /object creation
+		//new is a keyword
+		MethodsDiscussion xc=new MethodsDiscussion();
+				                 //Constructor
+				                 //Constructor is same as class name
+				
+		xc.andhrapradesh();
+		xc.nellore();
+		
+	
+		
 	}
 
 }
