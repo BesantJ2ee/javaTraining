69795520df69498b6069de4cc9bce6e39f0e682f
BesantJ2ee <simonjustin321@gmail.com>
2022-10-23 12:15:31 +0530

    DISCUSSION

---
+++ src/Oct_17/Table.java
@@ -0,0 +1,28 @@
+package Oct_17;
+
+public class Table {
+	int areaOfTable;
+	String unitMeasurement="mt";
+	int lengthOftheTable= 200;            
+    int widthoftheTable=100;
+    int heigthofthetable=50;
+    double priceOftheTable=12325;
+    String typeOfWood="rosewood";
+     String colourOftable="beige";
+     String materialofTable="solidwood";
+     int legOfTable=4;
+     String manufatureOfTable="ROYALoAK";
+      String address1OfManufautures="Orr ring road";
+     String address2OfManufacture="marthahalli";
+     String cityOfManufactures="banagalore";
+     String stateofManufactures="karnataka";
+     int pincodeOfmanufactures=574302;
+    int transporationCostOfProduct=2300;
+    double gstpercentageOftable=8.32;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
