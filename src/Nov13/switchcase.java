617c8aeb718d1026810c919942034bb699d20fbf
BesantJ2ee <simonjustin321@gmail.com>
2022-11-13 12:14:37 +0530

    switch statement

---
+++ src/Nov13/switchcase.java
@@ -0,0 +1,26 @@
+package Nov13;
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day = 10;
+			
+		switch (day-1) {
+		  case 2:
+		    System.out.println("Monday");
+		    break;
+		  case 4:
+		    System.out.println("Tuesday");
+		    break;
+		  case 7:
+		    System.out.println("Wednesday");
+		    break;
+		    default : 
+		    	System.out.println("The days execued");
+		    
+		  
+		}
+	}
+
+}
